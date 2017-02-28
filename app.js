ea3e90df9fe007a854d6def31c2b4055f6343bfc
amirOfficeLaptop <amir.modiri.master@gmail.com>
2017-02-28 09:25:43 +0300

    final

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-var express = require('express');
-var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose/');
-//var db = mongoose.connect('mongodb://dbUser:dbPassword@ds155428.mlab.com:55428/getir-bitaksi-hackathon');
-// var records = mongoose.model('records', yourSchema);
-var app = express();
+var express = require('express')
+  , bodyParser = require('body-parser')
+  , app = express()
+  , mongoose = require('mongoose/')
+  , Schema = mongoose.Schema
+  , Records = new Schema({
+      key:   {type: String, unique: true}
+      ,value:   String
+      ,createdAt:   Date
+  });
 
-app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/getRecord', function (req, res, next) {
-  var key = null;//'chp8vgSkJDbyDKAS';//req.body.key || null
+mongoose.connect('mongodb://dbUser:dbPassword@ds155428.mlab.com:55428/getir-bitaksi-hackathon');
 
-  if(!key) {
-    res.end('post is empty!');
-  }
+app.post('/getRecord', function (req, res, next) {
 
-  // var query = db.records.findOne({ 'key': key });
-  //
-  // query.exec(function (err, item) {
-  //   if (err) return handleError(err);
-  //   res.json(item);
-  // });
+    var key = req.body.key
+        , myRecords = mongoose.model('records', Records)
+        ;
+
+    if(!key) {
+       return res.end('post is empty!');
+    }
+
+    var query = myRecords.findOne({ 'key': key }, '-_id');
+
+    query.exec(function (err, item) {
+        if (err) return next(err);
+        res.json(item);
+    });
 });
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.end(err.message);
 });
 
-
-module.exports = app;
+app.listen(2020, function () {
+    console.log('Example app listening on port 3000!')
+});
